7b260b43189c41951362cede2de8a19957f32f93
Soumik <158482615+mallicksoumik1711@users.noreply.github.com>
2026-07-24 16:00:04 +0000

    any, unknown, never

--- src/TS_SCS/01_basicTypes.ts
+++ src/TS_SCS/01_basicTypes.ts
@@ -11,3 +11,54 @@ tuple = [2, "two", false]
 // --------------------------------------------------------------
 
 // Enums enumerations
+
+// - create properties
+// - no need to remenber them for future just access the enum and set
+
+enum UserRoles {
+    ADMIN = "admin",
+    GUEST = "guest",
+    SUPER_ADMIN = "super_admin"
+}
+
+UserRoles.ADMIN
+
+// - no numeric key in enums
+// - values can hav enums
+
+enum StatusCodes {
+    SUCCESS = 200,
+    NOT_FOUND = 404
+}
+
+StatusCodes.NOT_FOUND
+
+// ---------------------------------------------------------------
+
+// ANY vs UNKNOWN
+
+// any - any values can be assigned present / future
+// unknown - in future there will be some type assigned
+
+let a: any
+a = 10
+a = "some randomValue"
+
+a.toUpperCase()
+// no errors in compile time - but in runtime there will be 
+// should give error as uppercase cannot be assigned to number
+
+let b: unknown
+b = 20
+b = "random value"
+
+// b.toUpperCase()
+// here this will give error
+
+// ----------------------------------------------------------------------
+
+// NEVER
+
+// never returns anything 
+// infinite loop
+// throw errors
