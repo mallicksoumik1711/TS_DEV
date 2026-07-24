f9a98974a7f5813b28146d9d768c99494e7f3df5
Soumik <158482615+mallicksoumik1711@users.noreply.github.com>
2026-07-24 15:13:05 +0000

    basic types

---
+++ src/TS_SCS/01_basicTypes.ts
@@ -0,0 +1,13 @@
+// Primitive types - number, string, boolean, 
+// reference - (), [], {} -> where ever we can see these brackets
+
+// Tuples
+// - fixed size
+// - same format as described
+
+let tuple: [number, string, boolean?] = [12, "Alex"]
+tuple = [2, "two", false]
+
+// --------------------------------------------------------------
+
+// Enums enumerations
